6ef29f9f9ed55492f84b9d904fb48e42d1d76842
paniSil <mzwonko@gmail.com>
2024-12-09 19:11:16 +0200

    dark mode + js button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.html"],
+  darkMode: 'selector',
+  safeList:['bg-blue-400', 'translate-x-full', 'bg-pink-400', '-translate-x-2'],
   theme: {
     extend: {fontFamily:{
       main: ['Roboto', 'sans-serif'],
